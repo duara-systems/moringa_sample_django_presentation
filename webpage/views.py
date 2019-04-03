791c7085339b17c9221de0459d61caf1b5302ae6
Griffin <gmfalme@duara.io>
2019-04-03 19:10:49 +0300

    simple sample django webpage for deployment purposes

---
+++ webpage/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+# Create your views here.
+@require_http_methods(["GET"])
+def index(request):
+    return render(request, 'webpage/index.html')
